c13bb6dc904a955b1fadfc5b2674cec7e0019539
Michelle Li <michelle.mengqili@gmail.com>
2021-04-16 00:03:53 -0700

    made changes

--- ebay/src/main/java/com/Techbee/SeleniumTest/TargetTest.java
+++ ebay/src/main/java/com/Techbee/SeleniumTest/TargetTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TargetText {
+public class TargetTest {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
